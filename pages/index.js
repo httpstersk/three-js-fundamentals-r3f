da4773a4a440a0996d0e416f464fe4b03226918c
Lubos Belak <belak.lubos@gmail.com>
2020-11-12 19:33:34 +0100

    THREE.js Journey Start

---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>THREE DREI TRI</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <section className={styles.grid}>
+          <Link href="/fundamentals">
+            <a className={styles.card}>
+              <h3>THREE Day 1 &rarr;</h3>
+              <p>Fundamentals</p>
+            </a>
+          </Link>
+
+          <Link href="/primitives">
+            <a className={styles.card}>
+              <h3>THREE Day 2 &rarr;</h3>
+              <p>Primitives</p>
+            </a>
+          </Link>
+
+          <Link href="/scenegraph">
+            <a className={styles.card}>
+              <h3>THREE Day 3 &rarr;</h3>
+              <p>Scenegraph</p>
+            </a>
+          </Link>
+
+          <Link href="/materials">
+            <a className={styles.card}>
+              <h3>THREE Day 4 &rarr;</h3>
+              <p>Materials</p>
+            </a>
+          </Link>
+        </section>
+      </main>
+    </div>
+  );
+}
---
+++ pages/materials.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+
+export default function Materials() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>Materials</main>
+    </div>
+  );
+}
