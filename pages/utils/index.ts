716e0ef1e25a9db95f3fa4737f1a1fc66cab1214
Lubos Belak <belak.lubos@gmail.com>
2020-11-29 17:11:47 +0100

    :label: Add `MutableRefObject` type

--- pages/utils/index.ts
+++ pages/utils/index.ts
@@ -1,20 +1,28 @@
+import { MutableRefObject } from 'react';
+import { Mesh } from 'three';
 import { SPEED_ROTATION } from '../constants';
 import { randomColor } from 'randomcolor';
 
-export const rotateMesh = (meshRef) => {
+export const rotateMesh = (meshRef: MutableRefObject<Mesh>) => {
   if (meshRef.current) {
     meshRef.current.rotation.x += SPEED_ROTATION;
     meshRef.current.rotation.y += SPEED_ROTATION;
   }
 };
 
-export const rotateMeshX = (meshRef, speed = SPEED_ROTATION) => {
+export const rotateMeshX = (
+  meshRef: MutableRefObject<Mesh>,
+  speed = SPEED_ROTATION
+) => {
   if (meshRef.current) {
     meshRef.current.rotation.x += speed;
   }
 };
 
-export const rotateMeshY = (meshRef, speed = SPEED_ROTATION) => {
+export const rotateMeshY = (
+  meshRef: MutableRefObject<Mesh>,
+  speed = SPEED_ROTATION
+) => {
   if (meshRef.current) {
     meshRef.current.rotation.y += speed;
   }
