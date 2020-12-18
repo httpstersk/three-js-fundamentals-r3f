4cd3af5302a513567a3a4aebd9f3caac6e1add18
Lubos Belak <belak.lubos@gmail.com>
2020-12-18 21:08:35 +0100

    :sparkles: Controls for the `fov`, `far` and `near`

--- pages/07-cameras/index.tsx
+++ pages/07-cameras/index.tsx
@@ -1,5 +1,6 @@
 import { Suspense, useRef } from 'react';
 import { Canvas } from 'react-three-fiber';
+import { useTweaks } from 'use-tweaks';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { PerspectiveCamera as PerspectiveCameraType } from 'three';
 import { Cube, Plane, Sphere } from './components';
@@ -7,6 +8,11 @@ import { DirectionalLight } from './lights';
 
 export default function Scene() {
   const camera = useRef<PerspectiveCameraType>();
+  const { far, fov, near } = useTweaks('Camera', {
+    fov: { value: 45, min: 1, max: 180 },
+    near: { value: 0.1, min: 0.1, max: 50 },
+    far: { value: 100, min: 0.1, max: 100 },
+  });
 
   return (
     <Canvas>
@@ -18,10 +24,10 @@ export default function Scene() {
 
       <PerspectiveCamera
         aspect={2}
-        fov={45}
+        fov={fov}
         makeDefault
-        near={0.1}
-        far={100}
+        near={near}
+        far={far}
         position={[0, 10, 20]}
         ref={camera}
       />
