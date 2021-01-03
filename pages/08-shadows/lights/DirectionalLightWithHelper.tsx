72b489aba4dde549e2bc80aadb126564892e9c54
Lubos Belak <belak.lubos@gmail.com>
2021-01-03 10:30:16 +0100

    :sparkles: Controls for the shadow camera

--- pages/08-shadows/lights/DirectionalLightWithHelper.tsx
+++ pages/08-shadows/lights/DirectionalLightWithHelper.tsx
@@ -16,31 +16,53 @@ export default function DirectionalLightWithHelper() {
   const [isHelperShadowOn, toggleShadowHelper] = useState(true);
   const { scene } = useThree();
 
-  const { color, intensity, targetX, targetY, targetZ, x, y, z } = useTweaks(
-    'Directional Light',
-    {
-      color: CONSTANTS.DEFAULT_LIGHT_COLOR,
-      intensity: { value: CONSTANTS.DEFAULT_LIGHT_INTENSITY, min: 0, max: 2 },
-      ...makeFolder(
-        'Positions',
-        {
-          targetX: { value: -4, min: -10, max: 10 },
-          targetY: { value: 0, min: 0, max: 10 },
-          targetZ: { value: -4, min: -10, max: 10 },
-          x: { value: 0, min: -10, max: 10 },
-          y: { value: 10, min: 0, max: 10 },
-          z: { value: 10, min: -10, max: 10 },
-        },
-        true
-      ),
-      ...makeButton(`Toggle Light Helper`, () =>
-        toggleLightHelper((state) => !state)
-      ),
-      ...makeButton(`Toggle Shadow Helper`, () =>
-        toggleShadowHelper((state) => !state)
-      ),
-    }
-  );
+  const {
+    color,
+    intensity,
+    far,
+    height,
+    near,
+    targetX,
+    targetY,
+    targetZ,
+    width,
+    x,
+    y,
+    z,
+    zoom,
+  } = useTweaks('Directional Light', {
+    color: CONSTANTS.DEFAULT_LIGHT_COLOR,
+    intensity: { value: CONSTANTS.DEFAULT_LIGHT_INTENSITY, min: 0, max: 2 },
+    ...makeFolder(
+      'Shadow Camera',
+      {
+        width: { value: 100, min: 1, max: 100 },
+        height: { value: 100, min: 1, max: 100 },
+        near: { value: 0.1, min: 0.1, max: 50 },
+        far: { value: 50, min: 0.1, max: 50 },
+        zoom: { value: 1, min: 0.01, max: 1.5 },
+      },
+      true
+    ),
+    ...makeFolder(
+      'Positions',
+      {
+        targetX: { value: -4, min: -10, max: 10 },
+        targetY: { value: 0, min: 0, max: 10 },
+        targetZ: { value: -4, min: -10, max: 10 },
+        x: { value: 0, min: -10, max: 10 },
+        y: { value: 10, min: 0, max: 10 },
+        z: { value: 10, min: -10, max: 10 },
+      },
+      false
+    ),
+    ...makeButton(`Toggle Light Helper`, () =>
+      toggleLightHelper((state) => !state)
+    ),
+    ...makeButton(`Toggle Shadow Helper`, () =>
+      toggleShadowHelper((state) => !state)
+    ),
+  });
 
   const lightRef = useRef<DirectionalLight>();
   const targetRef = useRef<Object3D>();
@@ -58,7 +80,7 @@ export default function DirectionalLightWithHelper() {
     isHelperShadowOn && scene.add(cameraHelper);
 
     return () => scene.remove(cameraHelper);
-  }, [isHelperShadowOn, lightRef.current]);
+  }, [isHelperShadowOn, lightRef.current, far, near, zoom]);
 
   return (
     <group ref={targetRef}>
@@ -68,6 +90,11 @@ export default function DirectionalLightWithHelper() {
         intensity={intensity}
         position={[x, y, z]}
         ref={lightRef}
+        shadow-camera-far={far}
+        shadow-camera-near={near}
+        shadow-camera-zoom={zoom}
+        shadow-mapSize-height={height}
+        shadow-mapSize-width={width}
         target={targetRef.current}
       />
     </group>
