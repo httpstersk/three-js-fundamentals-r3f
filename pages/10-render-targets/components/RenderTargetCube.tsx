76d56a81fac7ee4ff883fb89d7d9aee9d268373a
Lubos Belak <belak.lubos@gmail.com>
2021-01-07 19:52:26 +0100

    :construction: <InnerScene>

--- pages/10-render-targets/components/RenderTargetCube.tsx
+++ pages/10-render-targets/components/RenderTargetCube.tsx
@@ -1,25 +1,27 @@
 import { useMemo, useRef } from 'react';
 import { createPortal, useFrame } from 'react-three-fiber';
 import { Box } from '@react-three/drei';
-import { Color, Scene, WebGLRenderTarget } from 'three';
+import { Color, Mesh, Scene, WebGLRenderTarget } from 'three';
 import { PerspectiveCamera as PerspectiveCameraType } from 'three';
 import PerspectiveCamera from '../cameras/PerspectiveCamera';
+import { rotateMesh } from '../../utils';
 
 const RenderTargetCube = ({ children }) => {
   const camera = useRef<PerspectiveCameraType>();
+  const mesh = useRef<Mesh>();
 
   const [scene, target] = useMemo(() => {
     const scene = new Scene();
-    scene.background = new Color('red');
+    scene.background = new Color('#FFFFFF');
     const target = new WebGLRenderTarget(512, 512);
     return [scene, target];
   }, []);
 
-  useFrame(({ clock, gl }) => {
-    // cam.current.position.z = 5 + Math.sin(clock.getElapsedTime() * 1.5) * 2;
+  useFrame(({ gl }) => {
     gl.setRenderTarget(target);
     gl.render(scene, camera.current);
     gl.setRenderTarget(null);
+    rotateMesh(mesh);
   });
 
   return (
@@ -34,9 +36,11 @@ const RenderTargetCube = ({ children }) => {
         position={[-1, 2, 4]}
       />
 
-      <Box args={[1, 1, 1]}>
-        <meshPhongMaterial color="blue" />
-      </Box>
+      <mesh ref={mesh}>
+        <Box args={[1, 1, 1]}>
+          <meshPhongMaterial color="red" map={target.texture} />
+        </Box>
+      </mesh>
     </>
   );
 };
--- pages/10-render-targets/index.tsx
+++ pages/10-render-targets/index.tsx
@@ -1,38 +1,53 @@
+import React, { Fragment } from 'react';
 import { Canvas } from 'react-three-fiber';
 import { OrbitControls } from '@react-three/drei';
 import { Cube, RenderTargetCube } from './components/';
-import PerspectiveCamera from './cameras/PerspectiveCamera';
+import { Color } from 'three';
 
 const CUBES = [
   {
     _id: 1,
-    color: 0x8844aa,
+    color: new Color(0x8844aa),
     x: -2,
   },
   {
     _id: 2,
-    color: 0x44aa88,
+    color: new Color(0x44aa88),
     x: 0,
   },
   {
     _id: 3,
-    color: 0xaa8844,
+    color: new Color(0xaa8844),
     x: 2,
   },
 ];
 
+type Cube = {
+  _id: number;
+  color: Color;
+  x: number;
+};
+
+type Props = {
+  cubes: Cube[];
+};
+
+const InnerScene = ({ cubes }: Props) => (
+  <Fragment>
+    {cubes.map(({ _id, color, x }) => {
+      return <Cube color={color} ndx={_id} key={_id} position-x={x} />;
+    })}
+
+    <directionalLight color={0xffffff} intensity={1.0} position={[-1, 2, 4]} />
+  </Fragment>
+);
+
 export default function Scene() {
   return (
     <Canvas>
-      <RenderTargetCube />
-
-      <directionalLight
-        color={0xffffff}
-        intensity={1.0}
-        position={[-1, 2, 4]}
-      />
-
-      <PerspectiveCamera />
+      <RenderTargetCube>
+        <InnerScene cubes={CUBES} />
+      </RenderTargetCube>
 
       <OrbitControls />
     </Canvas>
